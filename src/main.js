81c1d0b3d8b6fdf7d87ff770824e5a1dd38ed271
Maksym-Yaroshenko <yiaroshenko.max@gmail.com>
2024-04-01 01:35:50 +0300

    2131

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 import getPixabay from './js/pixabay-api';
+import renderInput from './js/render-functions';
 
 // Описаний у документації
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-console.log(getPixabay());
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// console.log(getPixabay());
 
 const searchForm = {
   form: document.querySelector('.search-form'),
   btn: document.querySelector('.btn-submit'),
+  ul: document.querySelector('.flex-container'),
 };
 
 searchForm.form.addEventListener('submit', handleSubmit);
@@ -18,16 +25,47 @@ function handleSubmit(event) {
   event.preventDefault();
   const user = event.target.query.value.trim();
   if (user !== '') {
-    getPixabay(user).then(data => {
-      if (data.hits.length !== 0) {
-        console.log(data);
-      } else {
-        iziToastIcon(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-        return searchForm.form.reset();
-      }
-    });
+    getPixabay(user)
+      .then(data => {
+        if (data.hits.length !== 0) {
+          // console.log(data.hits);
+          // for (let i = 0; i < data.hits.length; i += 1) {
+          //   console.log();
+          // }
+          searchForm.ul.innerHTML = '';
+          data.hits.map(
+            ({
+              webformatURL,
+              largeImageURL,
+              tags,
+              likes,
+              views,
+              comments,
+              downloads,
+            }) => {
+              searchForm.ul.insertAdjacentHTML(
+                'beforeend',
+                renderInput(
+                  webformatURL,
+                  largeImageURL,
+                  tags,
+                  likes,
+                  views,
+                  comments,
+                  downloads
+                )
+              );
+            }
+          );
+          return searchForm.form.reset();
+        } else {
+          iziToastIcon(
+            'Sorry, there are no images matching your search query. Please try again!'
+          );
+          return searchForm.form.reset();
+        }
+      })
+      .catch(error => console.error(error));
   } else {
     iziToastIcon('Please enter the text');
     return searchForm.form.reset();
@@ -45,3 +83,8 @@ function iziToastIcon(text) {
     close: false,
   });
 }
+
+new SimpleLightbox('.flex-container .retrieved-search-element a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
